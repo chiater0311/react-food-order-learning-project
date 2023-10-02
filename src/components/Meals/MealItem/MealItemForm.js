4ec54ee108a1f9bb643439fd25f8b58eaf60b643
chiater0311 <chiatertan@deegix.com>
2023-10-02 14:39:18 +0800

    second commit

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,23 +2,23 @@ import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
-    const amountInputRef = useRef();
+    // const amountInputRef = useRef();
 
     const submitHandler = event => {
         event.prevenDefault();
 
-        const enteredAmount = amountInputRef.current.value;
-        const enteredAmountNumber = +enteredAmount;
+        // const enteredAmount = amountInputRef.current.value;
+        // const enteredAmountNumber = +enteredAmount;
 
-        if (enteredAmount.trim().length === 0 || enteredAmount < 1 ) {
-            return;
-        }
+        // if (enteredAmount.trim().length === 0 || enteredAmount < 1 ) {
+        //     return;
+        // }
 
     }
 
     return <form className={classes.form} onSubmit={submitHandler}>
         <Input label="Amount" input={{
-            ref: {amountInputRef0},
+            // ref: {amountInputRef0},
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
